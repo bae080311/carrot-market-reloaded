64c6684869644cfd1235364b7630d949e6b1d725
bae080311 <s24024@gsm.hs.kr>
2025-06-10 08:25:57 +0900

    fix: font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
       <div className="w-full flex flex-col gap-3 items-center">
-        <Link className="primary-btn py-2.5" href="/create-account">
+        <Link className="primary-btn py-2.5 text-lg" href="/create-account">
           시작하기
         </Link>
         <div className="flex gap-2">
