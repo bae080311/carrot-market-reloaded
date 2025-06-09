b9ed8b0430bdc0e7450ac6dfddabb8d0d0bde41a
bae080311 <s24024@gsm.hs.kr>
2025-06-10 08:24:32 +0900

    refactor: btn 정의

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,7 @@ export default function Home() {
         <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
       <div className="w-full flex flex-col gap-3 items-center">
-        <Link
-          className="bg-orange-500 hover:bg-orange-400 font-medium w-full transition text-lg text-white text-center py-2.5 rounded-md"
-          href="/create-account"
-        >
+        <Link className="primary-btn py-2.5" href="/create-account">
           시작하기
         </Link>
         <div className="flex gap-2">
