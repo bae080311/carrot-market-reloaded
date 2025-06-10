92a29528a0ca580711a2af1e8f3a3646dbbf7210
bae080311 <s24024@gsm.hs.kr>
2025-06-10 17:58:51 +0900

    feat: login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import FormBtn from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
+
+export default function LogIn() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="Email" required error={[]} />
+        <FormInput type="password" placeholder="Password" required error={[]} />
+        <FormBtn text="Log in with email and password." loading={false} />
+      </form>
+      <SocialLogin />
+    </div>
+  );
+}
