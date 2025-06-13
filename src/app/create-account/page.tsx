b91bc0b70aacd8fcf28ed9f1fe6b3ab5e994ba2f
bae080311 <s24024@gsm.hs.kr>
2025-06-13 14:07:28 +0900

    fix: props

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,42 +1,47 @@
+"use client";
+
 import FormBtn from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { createAccount } from "./actions";
 
 export default function CreateAccount() {
+  const [state, action] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="username"
           type="text"
           placeholder="Username"
           required
-          error={["input error"]}
+          error={state?.fieldErrors.username}
         />
         <FormInput
           name="email"
           type="email"
           placeholder="Email"
+          error={state?.fieldErrors.email}
           required
-          error={["input error"]}
         />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
-          error={["input error"]}
+          error={state?.fieldErrors.password}
         />
         <FormInput
           name="confirmPassword"
           type="password"
           placeholder="Confirm Password"
           required
-          error={["input error"]}
+          error={state?.fieldErrors.confirmPassword}
         />
         <FormBtn text="Create account" />
       </form>
