fcd8653d9d9e62a2f924fa132b1ad51691fa517e
bae080311 <s24024@gsm.hs.kr>
2025-06-14 17:44:07 +0900

    feat: validate

--- src/app/create-account/actions.ts
+++ src/app/create-account/actions.ts
@@ -2,12 +2,38 @@
 
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(5).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirmPassword: z.string().min(10),
-});
+function checkUsername(username: string): boolean {
+  return !username.includes("potato");
+}
+
+function checkPassword({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}): boolean {
+  return password === confirmPassword;
+}
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        required_error: "사용자 이름은 필수입니다",
+        invalid_type_error: "사용자 이름은 문자열이어야 합니다",
+      })
+      .min(5, "너무 짧아")
+      .max(10, "너무 길어")
+      .refine(checkUsername, "custom error"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirmPassword: z.string().min(10),
+  })
+  .refine(checkPassword, {
+    message: "비밀번호와 비밀번호 확인이 다릅니다",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formdata: FormData) {
   const data = {
